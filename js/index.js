ffe6ff149d1b27a57314e7718ea00724eeaade6f
Victor Zhuravlev <mocambe@mail.ru>
2019-05-16 08:48:24 +0300

    uuid added

--- js/index.js
+++ js/index.js
@@ -1,11 +1,42 @@
 // visitorId must be set with cookie
-visitorId = 'superrandomsadfsdsdffID123'
+// visitorId = 'superrandomsadfsdsdffID123456790'
+let visitorId;
+
+function makeNewId() {
+	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+		);
+};
+	
+function getCookie(name) {
+	var value = "; " + document.cookie;
+	var parts = value.split("; " + name + "=");
+	if (parts.length == 2) return parts.pop().split(";").shift();
+  }
+	
+function proceedNewVisitor() {
+	visitorId = makeNewId();
+	document.cookie= 'userId=' + visitorId;
+};
+
+function proceedReturningVisitor() {
+	visitorId = getCookie('userId');
+}
+		
+function setUserId() {
+	if(getCookie('userId') !== undefined) {
+		visitorId = getCookie('userId');
+	} else {
+		proceedNewVisitor();
+	}
+}
+
+setUserId();
+console.log(visitorId);
+		
 token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZhbnR5czIwMTFAZ21haWwuY29tIiwiaXNTdXBlckFkbWluIjp0cnVlLCJpYXQiOjE1NTc1ODQyMTcsImV4cCI6MTU1NzYwNTgxNywiYXVkIjoid2ViLWxvZ2luIn0.fD9AI0KpoMSuh8NvxeSBAgw3aJKiFo8YeIFzWpaHB4c'
 
-
 BOT_URL = 'http://bot.bad.family/api/v1/bots/badtest/mod/channel-web/messages/' + visitorId
-
-
 var guest = io('http://bot.bad.family/guest?visitorId=' + visitorId);
 var admin = io('http://bot.bad.family/admin?visitorId=' + visitorId + '&token=' + token, { forceNew: true });
 
@@ -196,10 +227,14 @@ function render_file_msg(msg) {
 	//		url: url
 
 			
-	
-    $('.chat-messages').append('<div class="message inbox"><div class="text"><img src="'+msg.data.payload.url+'"></div></div>')
+	if(msg.data.payload.url.slice(-3) === 'jpg' || msg.data.payload.url.slice(-4) === 'jpeg') {
+		$('.chat-messages').append('<div class="message inbox"><div class="text"><img src="'+msg.data.payload.url+'"></div></div>');
+		console.log('picture');
+	} else {
+		$('.chat-messages').append('<div class="message inbox"><a class="link_pdf" href="'+msg.data.payload.url+'"><img class="img_pdf" src="./images/pdf.svg" alt="pdf.file"><br>Скачать</a></div>');
+		console.log('pdf');
+	}
 }
-
 
 function render_text_msg(msg, sender) {
     if (sender == 'bot') {
